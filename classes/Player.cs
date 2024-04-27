5fffb729c3afc5406cc65253947afdc779e4f7fb
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-04-27 17:01:58 +0200

    add player class with name and age properties

---
+++ classes/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracownikSadu.classes
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Player(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
