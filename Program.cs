e060b376c0e0a5d7df9a71fb40d0a407a60d2f75
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-04-27 17:30:10 +0200

    add GetUserInt method, that asks a user for an age, then returns it

--- Program.cs
+++ Program.cs
@@ -7,12 +7,51 @@ namespace PracownikSadu
         {
             Console.WriteLine("Welcome in my game!");
             string name = GetUserInput("How would you like to be called? ");
+
         }
         static string GetUserInput(string message)
         {
             Console.Write(message);
             string? result = Console.ReadLine();
             return result ?? "";
+        }
+        static int GetUserInt(string message)
+        {
+            int result;
+            Console.Write(message);
+            do
+            {
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                    if (result < 18)
+                    {
+                        Console.WriteLine("You are too young for getting a job!");
+                    }
+                    else if (result > 65)
+                    {
+                        Console.WriteLine("Go on retirement old man!");
+                    }
+                    return result;
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                    Console.WriteLine("Try to pass a value that is between 18 and 65");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                    Console.WriteLine("You have to pass only a number");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                    Console.WriteLine("idk sth went wrong . . .");
+                }
+            }
+            while(true);
+            
         }
     }
 }
