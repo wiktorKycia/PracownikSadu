8e4a646e7c76c6aa35883e351e566b75984d9714
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-04-28 13:36:31 +0200

    to Player class, add nullable property Job, that is set to null by default

--- classes/Player.cs
+++ classes/Player.cs
@@ -11,6 +11,7 @@ namespace PracownikSadu.classes
         public string Name { get; set; }
         public int Age { get; set; }
         public int Money { get; set; }
+        public Job? Job { get; set; } = null;
         public Player(string name)
         {
             Name = name;
