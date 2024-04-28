f12c6b98bbe70b5efabe3c9f36025f0ee37b73f3
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-04-28 12:55:18 +0200

    create a new class: Job

---
+++ classes/Job.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracownikSadu.classes
+{
+    internal class Job
+    {
+    }
+}
