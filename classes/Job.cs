ba6cf95b8c1abd124492b2c273aca021ce79d58f
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-05-15 19:09:05 +0200

    Update class Job, set BonusMin to 0 at first, add experience and skill both gain and requirements, update the PresentJobOffer method to have this information

--- classes/Job.cs
+++ classes/Job.cs
@@ -9,10 +9,15 @@ namespace PracownikSadu.classes
     internal class Job
     {
         public int Salary { get; set; }
-        public int BonusMin { get; set; }
+        public int BonusMin { get; set; } = 0;
         public int BonusMax { get; set; }
         public string Name { get; set; }
         public string Presentation { get ; set; }
+
+        public int ExperienceGain { get; set; }
+        public int SkillGain { get; set; }
+        public int ExperienceRequirements { get; set; }
+        public int SkillRequirements { get; set; }
 
         public Job(string name, int salary, int bonusMax, string presentation)
         {
@@ -34,11 +39,18 @@ namespace PracownikSadu.classes
         public void PresentJobOffer()
         {
             Console.WriteLine($"{Name}:\n" + Presentation);
+            Console.WriteLine($"You can improve your skills by {SkillGain}\n and gain {ExperienceGain} experience per turn");
             Console.WriteLine($"We offer the stable salary of ${Salary}");
-            if (BonusMin!=0)
+            if (BonusMin > 0)
+            {
                 Console.WriteLine($"We also offer bonuses from ${BonusMin} up to ${BonusMax} \n");
+            }
             else
+            {
                 Console.WriteLine($"We also offer bonuses up to ${BonusMax} \n");
+            }
+            Console.WriteLine($"However, we require from you to have skill above {SkillRequirements}\n" +
+                $"And experience greater than {ExperienceRequirements}");
         }
     }
 }
